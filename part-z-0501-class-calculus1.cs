d23fc6b93705af7dd1d359d76c016cbc2362fbd6
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-05-08 09:24:00 +0300

    Create part-z-0501-class-calculus1.cs

---
+++ part-z-0501-class-calculus1.cs
@@ -0,0 +1,15 @@
+Calculus c = new Calculus(); // Calculus class creation started...
+
+
+
+
+class Calculus
+{
+    public Calculus() // constructor
+    {
+        System.Console.WriteLine("Calculus class creation started...");
+    }
+    
+}
+
+// dotnet run
