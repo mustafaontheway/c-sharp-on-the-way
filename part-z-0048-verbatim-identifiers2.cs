e461ce116edf7d05ed74ba5eead947d214a007dc
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-05-13 19:39:43 +0300

    Create part-z-0048-verbatim-identifiers2.cs

---
+++ part-z-0048-verbatim-identifiers2.cs
@@ -0,0 +1,19 @@
+/*
+Python code: 
+
+class ModelResult:
+    def __init__(self):
+        # in Python 'event' is not reserved
+        self.event = "Financial Crisis 2026" 
+        self.value = 0.85
+
+*/
+
+// var pythonResult = PythonEngine.GetModelResult();
+
+// Console.WriteLine(pythonResult.event); // Error! -> "event" is reserved!
+
+// Console.WriteLine(pythonResult.@event);
+
+
+// dotnet run
